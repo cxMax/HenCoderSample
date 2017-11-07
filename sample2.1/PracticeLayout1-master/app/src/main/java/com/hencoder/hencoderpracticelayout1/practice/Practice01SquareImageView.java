bf935061f6049fb06ea2b3271dc7e39d76ee3ce6
cxmax <caixi169@hotmail.com>
2017-11-07 21:53:06 +0800

    1. update 2.1 sample

---
+++ sample2.1/PracticeLayout1-master/app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
@@ -0,0 +1,61 @@
+package com.hencoder.hencoderpracticelayout1.practice;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * 需要把它写成正方形的 ImageView
+ */
+public class Practice01SquareImageView extends ImageView {
+    public Practice01SquareImageView(Context context) {
+        super(context);
+    }
+
+    public Practice01SquareImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice01SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * 重写 onMeasure()，调整尺寸，让 ImageView 总是正方形
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        // 先用 getMeasuredWidth() 和 getMeasuredHeight() 取到 super.onMeasure() 的计算结果
+
+        // 然后通过计算，让宽度和高度一致
+
+        // 再用 setMeasuredDimension(width, height) 来保存最终的宽度和高度
+        int width = measureSize(widthMeasureSpec, getMeasuredWidth());
+        int height = measureSize(heightMeasureSpec, getMeasuredHeight());
+
+        if (width > height) {
+            setMeasuredDimension(height, height);
+        } else {
+            setMeasuredDimension(width, width);
+        }
+    }
+
+    private int measureSize(int measureSpec , int defaultSize) {
+        int result = defaultSize;
+        int specMode = View.MeasureSpec.getMode(measureSpec);
+        int specSize = View.MeasureSpec.getSize(measureSpec);
+        switch (specMode) {
+            case View.MeasureSpec.UNSPECIFIED:
+            case View.MeasureSpec.AT_MOST:
+                break;
+            case View.MeasureSpec.EXACTLY:
+                result = specSize;
+                break;
+        }
+        return result;
+    }
+}
